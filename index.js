80341d003bda0cb7fd651677268962c56dfbc2c5
AlexDanchenkoIT <125401462+AlexDanchenkoIT@users.noreply.github.com>
2023-06-17 16:39:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,54 +4,60 @@ const inputFromUserNode = document.querySelector('.js-input');
 const btnAddMovieNode = document.querySelector('.js-button');
 const listMoviesNode = document.querySelector('.js-movies-list');
 const buttonsCloseNode = document.querySelector('.js-movies-list');
-const counterInputLengthNode = document.querySelector('.js-counter-input')
-const counterInputNode = document.querySelector('.js-input-length')
+const counterInputLengthNode = document.querySelector('.js-counter-input');
+const counterInputNode = document.querySelector('.js-input-length');
 
 btnAddMovieNode.addEventListener('click', function(){
 
     const movieFromUser = getMovieFromUser();
 
     if (!movieFromUser) {
-        return
+        return;
     }
 
     if ((movieFromUser.length > MAX_LENGTH_INPUT)) {
         return;
     }
 
-    renderMovies()
-    clearInput()
-    clearCounter()
+    renderMovies();
+    clearInput();
+    clearCounter();
 })
 
 listMoviesNode.addEventListener('click', function(e) {
   if (e.target.closest('.close-btn')){
-      e.target.closest('.movie').remove()
-    };
+      e.target.closest('.movie').remove();
+    }
 });
 
 listMoviesNode.addEventListener('click', function(event){
-    if (event.target.closest('.input-radio')){
-        event.target.closest('.movie').classList.toggle('input-radio_done')
-      };
-})
+    if (event.target.closest('.input-checkbox')){
+        event.target.closest('.movie').classList.toggle('input-checkbox_done');
+      }
+});
+
+inputFromUserNode.addEventListener("keyup", function(e) {
+    if (e.code === 'Enter') {
+        btnAddMovieNode.click();
+    }
+});
 
 inputFromUserNode.addEventListener('input', onInput);
 
 
 function getMovieFromUser(){
-    const getMovie = inputFromUserNode.value
-    const newMovie = getMovie.trim()
-    return newMovie
+    const getMovie = inputFromUserNode.value;
+    const newMovie = getMovie.trim();
+    return newMovie;
 }
 
 function createMovieCard(movie) {
-    const movieCard = document.createElement('li')
-    movieCard.classList.add('movie')
+    const movieCard = document.createElement('li');
+    movieCard.classList.add('movie');
 
     movieCard.innerHTML = `
         <label>
-        <input class="input-radio" type="checkbox" data-action="done" />
+        <input class="input-checkbox" type="checkbox" data-action="done" />
         <p class="title-movie">${movie}</p>
         </label>
         <button class="close-btn" data-action="delete">
@@ -61,28 +67,26 @@ function createMovieCard(movie) {
            alt="изобращение крестика"/>
        </button>
        `
-    return movieCard
+    return movieCard;
 }
 
 function renderMovies(){
-    const movie = getMovieFromUser()
-    const movieCard = createMovieCard(movie)
-    listMoviesNode.prepend(movieCard) 
-
+    const movie = getMovieFromUser();
+    const movieCard = createMovieCard(movie);
+    listMoviesNode.prepend(movieCard);
 }
 
 function clearInput(){
-    inputFromUserNode.value = ''
+    inputFromUserNode.value = '';
 }
 
 function clearCounter (){
-    counterInputLengthNode.innerText = 0;
-    
+    counterInputLengthNode.innerText = 0; 
 }
 
 function onInput(evt){
-    let length = evt.target.value.length
-    counterInputLengthNode.innerText = length
+    let length = evt.target.value.length;
+    counterInputLengthNode.innerText = length;
     
     if (length > MAX_LENGTH_INPUT) {
         counterInputNode.classList.add('limit-exhausted');
@@ -90,11 +94,3 @@ function onInput(evt){
         counterInputNode.classList.remove('limit-exhausted');
     }
 }
-
-
-
-
-
-
-
-
